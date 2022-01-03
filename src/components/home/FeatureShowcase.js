ac35f105fdfe8a165168e06b1344462c02b20d71
Greyson Murray <greyson.murray@gmail.com>
2022-01-02 21:51:10 -0500

    add inner container

--- src/components/home/FeatureShowcase.js
+++ src/components/home/FeatureShowcase.js
@@ -4,10 +4,12 @@ function FeatureShowcase(props) {
     const feature = props.feature
     return (
         <div className='feature'>
-            <div className='feature__info pr-xxl p-md'>
-                <h5 className='feature__name h5-spaced mb-xs'>{ feature.name }</h5>
-                <h2 className='feature__hook mb-sm'>{ feature.hook }</h2>
-                <p className='p-body-sm'>{ feature.description }</p>
+            <div className='feature__info p-md'>
+                <div className='feature__info__inner'>
+                    <h5 className='feature__name h5-spaced mb-xs'>{ feature.name }</h5>
+                    <h2 className='feature__hook mb-sm'>{ feature.hook }</h2>
+                    <p className='p-body-sm'>{ feature.description }</p>
+                </div>
             </div>
             <div className='feature__thumbnail'>
                 <img className='feature__img' alt={ feature.imgAlt } src={ feature.img } />
