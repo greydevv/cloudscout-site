21bf05dbbf61b19ab8c3a3d3e93b5704d0d62731
Greyson Murray <greyson.murray@gmail.com>
2022-01-05 10:11:04 -0500

    update Auth0Provider props

--- src/index.js
+++ src/index.js
@@ -18,9 +18,11 @@ import './index.scss';
 ReactDOM.render(
     <Router>
         <Auth0Provider
-            domain={ process.env.REACT_APP_AUTH0_DOMAIN }
             clientId={ process.env.REACT_APP_AUTH0_CLIENT_ID }
-            redirectUri={window.location.origin}
+            domain={ process.env.REACT_APP_AUTH0_DOMAIN }
+            audience={ process.env.REACT_APP_AUTH0_AUDIENCE }
+            scope='read:players'
+            redirectUri={ window.location.origin }
         >
             <Routes>
                 <Route path='/' element={ <Home /> }>
