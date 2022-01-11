819858c4fd218de9551d91fbeeb12c21ba3ad33a
Greyson Murray <greyson.murray@gmail.com>
2022-01-11 14:42:05 -0500

    update useApi hook for dynamic reloading

--- src/api/api.js
+++ src/api/api.js
@@ -2,30 +2,34 @@ import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const useApi = (endpoint) => {
-    const url = `https://cloudscout-rest.herokuapp.com/${endpoint}`;
-    // const url = `http://127.0.0.1/${endpoint}` 
-    const [data, setData] = useState({json: null, isLoading: true});
+    const BASE_URL = 'https://cloudscout-rest.herokuapp.com/';
+    const [url, setUrl] = useState(`${BASE_URL}${endpoint}`);
+    const [data, setData] = useState({json: [], isLoading: true});
     const { getAccessTokenSilently } = useAuth0();
 
     useEffect(() => {
+        if (url === BASE_URL) {
+            return;
+        }
         setData(data => ({json: data.json, isLoading: true}));
         const getData = async () => {
             const accessToken = await getAccessTokenSilently({
                 audience: process.env.REACT_APP_AUTH0_AUDIENCE,
                 scope: 'read:players'
             });
-
-            console.log(accessToken);
             const apiResponse = await fetch(url, {
                 headers: {
                     Authorization: `Bearer ${accessToken}`,
                 }
             });
-
             setData({json: await apiResponse.json(), isLoading: false});
         };
         getData();
     }, [url]);
+
+    useEffect(() => {
+        setUrl(`${BASE_URL}${endpoint}`)
+    }, [endpoint]);
 
     return data;
 }
