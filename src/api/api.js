d06060893a07932d60bba36097473af3ff1478d4
Greyson Murray <greyson.murray@gmail.com>
2022-01-21 10:39:10 -0500

    implement usePut for put requests

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,61 @@
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
+const BASE_URL = 'https://cloudscout-rest.herokuapp.com';
+
+export const usePut = (endpoint, body, enabled=true) => {
+    const { getAccessTokenSilently } = useAuth0();
+    const [response, setResponse] = useState({json: {}, code: 200, isLoading: true});
+    const [url, setUrl] = useState(`${BASE_URL}${endpoint}`);
+    const [firstRender, setFirstRender] = useState(true);
+
+    useEffect(() => {
+        if (!enabled || url === BASE_URL || Object.keys(body).length === 0) {
+            // prevent useless requests
+            return;
+        }
+        if (firstRender) {
+            // don't make request on first render
+            setFirstRender(false);
+            return;
+        }
+        setResponse(response => ({json: response.json, code: response.code, isLoading: true}))
+        const makeRequest = async () => {
+            const accessToken = await getAccessTokenSilently({
+                audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+                scope: 'read:players'
+            });
+
+            let reqObj = {
+                'method': 'PUT',
+                'headers': {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'Application/JSON'
+                }
+            }
+
+            if (body !== undefined) {
+                reqObj['body'] = JSON.stringify(body);
+            }
+
+            const apiResponse = await fetch(url, reqObj);
+
+            setResponse({
+                json: apiResponse.status !== 204 ? await apiResponse.json() : {}, 
+                code: apiResponse.status, 
+                isLoading: false
+            });
+        }
+        makeRequest();
+    }, [url, body]);
+
+    return response;
+}
+
 export const useApi = (endpoint, enabled=true) => {
     const BASE_URL = 'https://cloudscout-rest.herokuapp.com/';
     const [url, setUrl] = useState(`${BASE_URL}${endpoint}`);
-    const [data, setData] = useState({json: {}, isLoading: true});
+    const [data, setData] = useState({json: {}, code:200, isLoading: true});
     const { getAccessTokenSilently } = useAuth0();
 
     useEffect(() => {
@@ -14,7 +65,7 @@ export const useApi = (endpoint, enabled=true) => {
         if (url === BASE_URL) {
             return;
         }
-        setData(data => ({json: data.json, isLoading: true}));
+        setData(data => ({json: data.json, code: data.code, isLoading: true}));
         const getData = async () => {
             const accessToken = await getAccessTokenSilently({
                 audience: process.env.REACT_APP_AUTH0_AUDIENCE,
@@ -25,7 +76,7 @@ export const useApi = (endpoint, enabled=true) => {
                     Authorization: `Bearer ${accessToken}`,
                 }
             });
-            setData({json: await apiResponse.json(), isLoading: false});
+            setData({json: await apiResponse.json(), code: apiResponse.status, isLoading: false});
         };
         getData();
     }, [url]);
