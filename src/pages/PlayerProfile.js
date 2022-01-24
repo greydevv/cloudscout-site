bc7431553dd3a1ecc411bc0219933bcbff09e30f
Greyson Murray <greyson.murray@gmail.com>
2022-01-24 13:43:51 -0500

    refactor

--- src/pages/PlayerProfile.js
+++ src/pages/PlayerProfile.js
@@ -14,7 +14,7 @@ function StatRow({ stat, value, classAvgStat, allAvgStat }) {
     const nullify = (allAvgStat === null || allAvgStat === 0 || value === null || value === 0);
     const isPositive = value > allAvgStat;
     const pct = (nullify) ? null : parseInt((100 * (value - allAvgStat)) / allAvgStat);
-    const pctText = (pct === null || (pct < 1 && pct > 0)) ? '-' : `${Math.abs(parseInt(pct))}%`;
+    const pctText = (pct === null || (pct < 1 && pct > 0)) ? 'N/A' : `${Math.abs(parseInt(pct))}%`;
 
     const getArrow = () => {
         return (isPositive ? <ArrowUp /> : <ArrowDown />);
@@ -26,7 +26,7 @@ function StatRow({ stat, value, classAvgStat, allAvgStat }) {
                 <h4 className='stat__name my-auto'>{ prettifyText(stat).pretty }</h4>
             </td>
             <td className='stat__col'>
-                <p className='stat__metric__text'>{ value === null ? '-' : value }</p>
+                <p className='stat__metric__text'>{ value === null ? 'N/A' : value }</p>
             </td>
             <td className='stat__col'>
                 {/*<p className='stat__metric__name p-body-sm'>All</p>*/}
@@ -36,6 +36,14 @@ function StatRow({ stat, value, classAvgStat, allAvgStat }) {
                 </div>
             </td>
         </tr>
+    );
+}
+
+function NoStats() {
+    return (
+        <div className='profile__statistics__container-nodata'>
+            <p className='profile__statistics__container-nodata__text'>No stats</p>
+        </div>
     );
 }
 
@@ -55,7 +63,7 @@ export default function PlayerProfile() {
     }
 
     const position = isLoading ? '' : json.meta.position;
-    const { json: averageJson, isLoading: isAverageLoading } = useApi(`v1/analysis${makePosition(position)}`, true, false);
+    const { json: averageJson, isLoading: isAverageLoading } = useApi(`v1/analysis${makePosition(position)}`, position !== null, false);
     const [currentTab, setCurrentTab] = useState('general');
 
     useEffect(() => {
@@ -68,13 +76,8 @@ export default function PlayerProfile() {
         setCurrentTab(category);
     };
 
-    if (isLoading || isUserLoading || isAverageLoading) {
-        return <SpinnerView />
-    }
-
     const onFavorite = (pid) => {
         userJson.account.favorites = [...userJson.account.favorites, pid];
-        // setUserState({favorites: [...userJson.account.favorites]})
         setJsonBody(copyObj(userJson));
     }
 
@@ -87,13 +90,18 @@ export default function PlayerProfile() {
             }
         }
         userJson.account.favorites = newFavorites;
-        // setUserState({favorites: newFavorites})
         setJsonBody(copyObj(userJson));
     }
+
+    if (isLoading || isUserLoading || (isAverageLoading && position !== null)) {
+        return <SpinnerView />
+    }
     
-    const statTabs = json.stats;
-    const inputJson = statTabs[currentTab];
     const isFavorited = userJson.account.favorites.includes(json.pid);
+    const statTabs = json.stats;
+
+    const hasNoStats = Object.keys(averageJson).length === 0;
+    const inputJson = hasNoStats ? null : statTabs[currentTab];
 
     return (
         <>
@@ -109,38 +117,45 @@ export default function PlayerProfile() {
                                <StarFilled className='favorite__btn__icon' />
                            </button>)
                     }
-                    <div className='profile__categories'>
-                        { Object.keys(statTabs).map((category, i) => {
-                            const isCurrentCategory = category === currentTab;
-                            return (
-                                <div key={ i } className={ isCurrentCategory ? 'profile__categories__tab-active' : 'profile__categories__tab'}>
-                                    <a
-                                        className={ 'p-body-sm profile__categories__link' + (isCurrentCategory ? '-active' : '') }
-                                        onClick={() => changeCurrentTab(category)}
-                                    >
-                                        { category.toUpperCase() }
-                                    </a>
-                                </div>
-                            );
-                        })}
-                    </div>
                 </div>
-                <div className='profile__statistics__container py-lg'>
-                    <table className='profile__statistics__table'>
-                        <tbody>
-                            { Object.entries(inputJson).map(([stat, value], i) => {
-                                return (
-                                    <StatRow 
-                                        key={ i } 
-                                        stat={ stat } 
-                                        value={ value } 
-                                        allAvgStat={ averageJson[currentTab][stat] } 
-                                    />
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                {hasNoStats
+                    ? <NoStats />
+                    : (
+                        <>
+                            <div className='profile__categories'>
+                                { Object.keys(statTabs).map((category, i) => {
+                                    const isCurrentCategory = category === currentTab;
+                                    return (
+                                        <div key={ i } className={ isCurrentCategory ? 'profile__categories__tab-active' : 'profile__categories__tab'}>
+                                            <a
+                                                className={ 'p-body-sm profile__categories__link' + (isCurrentCategory ? '-active' : '') }
+                                                onClick={() => changeCurrentTab(category) }
+                                            >
+                                                { category.toUpperCase() }
+                                            </a>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <div className='profile__statistics__container py-lg'>
+                                <table className='profile__statistics__table'>
+                                    <tbody>
+                                        { Object.entries(inputJson).map(([stat, value], i) => {
+                                            return (
+                                                <StatRow 
+                                                    key={ i } 
+                                                    stat={ stat } 
+                                                    value={ value } 
+                                                    allAvgStat={ averageJson[currentTab][stat] } 
+                                                />
+                                            );
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </>
+                    )
+                }
             </div>
         </>
     );
