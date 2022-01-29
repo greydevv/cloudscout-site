edbf2ceb7f7a500bcea36b31db03023964dea41c
Greyson Murray <greyson.murray@gmail.com>
2022-01-29 18:05:17 -0500

    support useRest & changes to useRest

--- src/api/useRest.js
+++ src/api/useRest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const restApi = axios.create({baseUrl: 'https://cloudscout-rest.herokuapp.com/'});
+const restApi = axios.create({baseURL: 'https://cloudscout-rest.herokuapp.com/'});
 
 export const usePut = (url, body, enabled=true) => {
     const config = {
@@ -22,33 +22,60 @@ export const useGet = (url, enabled=true) => {
     return useRest(config, enabled);
 }
 
-export const useRest = (config, enabled=true) => {
+export const useRest = (config, enabled=true, defaultJson={}, enabledOnFirstRender=true) => {
     const { getAccessTokenSilently } = useAuth0();
-    const [response, setResponse] = useState({json: {}, code: 200, isLoading: enabled});
+    const [response, setResponse] = useState({json: defaultJson, code: 200, isLoading: enabled});
+    const [firstRender, setFirstRender] = useState(true);
 
     useEffect(() => {
+        if (firstRender && !enabledOnFirstRender) {
+            setFirstRender(false);
+            return;
+        }
         if (!enabled) {
             return;
         }
         const makeHttpReq = async () => {
             const accessToken = await getAccessTokenSilently({
-                audience: process.env.AUTH0_REACT_APP_AUDIENCE,
+                audience: process.env.REACT_APP_AUTH0_AUDIENCE,
                 scope: 'read:players'
             });
-            const authHeaders = {'Authorzation': `Bearer ${accessToken}`};
             const apiResponse = await restApi.request({
                 ...config, 
                 headers: {
                     ...config.headers, 
-                    authHeaders
+                    'Authorization': `Bearer ${accessToken}`
                 }
             });
             setResponse({ 
-                json: apiResponse.data,
+                json: await apiResponse.data,
                 code: apiResponse.status,
                 isLoading: false
             });
         }
         makeHttpReq();
-    }, [config.url, enabled]);
+    }, [config.url, config.params, enabled]);
+
+    return response;
 }
+
+export const useHttpReq = () => {
+    const { getAccessTokenSilently } = useAuth0();
+    const makeHttpReq = async (config) => {
+        const accessToken = await getAccessTokenSilently({
+            audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+            scope: 'read:players'
+        });
+        return await restApi.request({
+            ...config,
+            headers: {
+                ...config.headers,
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'Application/JSON',
+            }
+        });
+    }
+
+    return makeHttpReq;
+}
+
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import PlayerTable from '../components/PlayerTable';
 import BasePlayer from '../models/Player';
-import { useApi, usePut }  from 'api/api.js';
+import { useApi, usePut }  from 'api/api';
 import { useUserContext } from 'UserContext';
 import { copyObj } from 'util/utils';
 import { SpinnerView } from 'components/Spinner';
 import './Favorites.scss';
+
+// import { useRest } from 'api/useRest';
+
+// export default function Favorites() {
+//     const [favoritePids, setFavoritePids] = useState([]);
+//     const { json: user, isLoading: isUserLoading } = useRest({url: `v1/users/${useUserContext()}`});
+//     const { json: players, isLoading: isPlayersLoading } = useRest({
+//         url: `v1/players`, 
+//         params: {pids: favoritePids.join(',')}
+//     }, !isUserLoading && favoritePids.length > 0)
+
+//     useEffect(() => {
+//         if (!isUserLoading) {
+//             setFavoritePids(user.account.favorites);
+//             console.log(favoritePids);
+//         }
+//     }, [isUserLoading]);
+
+//     const onFavorite = (pid) => {
+//         setFavoritePids([...favoritePids, pid])
+//         console.log('onFavorite: ', favoritePids)
+//     }
+
+//     const onUnfavorite = (pid) => {
+//         let newFavorites = favoritePids;
+//         for (var i = newFavorites.length - 1; i >= 0; i--) {
+//             if (newFavorites[i] === pid) {
+//                 newFavorites.splice(i, 1);
+//                 break;
+//             }
+//         }
+//         setFavoritePids(newFavorites);
+//         console.log('onUnfavorite: ', favoritePids);
+//     }
+
+//     return (
+//         <div className='favorites'>
+//             <div className='page__header'>
+//                 <h1 className='page__head mb-md'>Favorites</h1>
+//             </div>
+//             <PlayerTable 
+//                 onFavorite={ onFavorite }
+//                 onUnfavorite={ onUnfavorite } 
+//                 favorites={ favoritePids } 
+//                 players={ players.length === 0 ? [] : (isPlayersLoading ? [] : players.map(BasePlayer.fromJson)) } 
+//                 isLoading={ initialPids.length !== 0 && isPlayersLoading } 
+//             />
+//         </div>
+//     );
+// }
 
 export default function Favorites() {
     const userId = useUserContext();
