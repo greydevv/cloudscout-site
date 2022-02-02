485c84ee98fa40b5ca11b1885bfa075084f98b8f
Greyson Murray <greyson.murray@gmail.com>
2022-02-01 22:22:36 -0500

    visual changes

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -6,9 +6,12 @@ import { ReactComponent as Logout } from 'assets/icons/logout.svg';
 import { ReactComponent as Search } from 'assets/icons/search.svg';
 import { ReactComponent as LoadingInner } from 'assets/icons/loadingInner.svg';
 import { ReactComponent as Checkmark } from 'assets/icons/checkmark.svg';
+import { ReactComponent as CheckmarkSmall } from 'assets/icons/checkmarkSmall.svg';
 import { ReactComponent as Crown } from 'assets/icons/crown.svg';
 import { ReactComponent as Burger } from 'assets/icons/burger.svg';
 import { ReactComponent as Close } from 'assets/icons/close.svg';
+import { ReactComponent as CloseSmall } from 'assets/icons/closeSmall.svg';
+import { ReactComponent as PlusSmall } from 'assets/icons/plusSmall.svg';
 import { ReactComponent as ArrowDown } from 'assets/icons/arrowDown.svg';
 import { ReactComponent as ArrowUp } from 'assets/icons/arrowUp.svg';
 
@@ -20,8 +23,11 @@ export { Dashboard,
          Search,
          LoadingInner,
          Checkmark,
+         CheckmarkSmall,
          Crown,
          Burger,
          Close,
+         CloseSmall,
+         PlusSmall,
          ArrowDown,
          ArrowUp }
